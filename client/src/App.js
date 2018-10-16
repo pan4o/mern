50bf9fb995480c6b19f009049a61aeacce15eea3
pan4o <panchenko923@gmail.com>
2018-10-16 23:55:36 +0300

    start create profile

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import Footer from './components/layout/Footer';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
 
 import PrivateRoute from './components/common/PrivateRoute';
 
@@ -52,6 +53,9 @@ class App extends Component {
               <Route exact path="/login" component={Login} />
               <Switch>
                 <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              </Switch>
+              <Switch>
+                <PrivateRoute exact path="/create-profile" component={CreateProfile} />
               </Switch>
             </div>
             <Footer />
