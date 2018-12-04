e8b4814b0ea7027ce362fb2b79b858bf93dbdef8
pan4o <panchenko923@gmail.com>
2018-12-05 00:51:43 +0200

    add post reducer

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,12 @@
+const initailState = {
+  posts: [],
+  post: {},
+  loading: false
+};
+
+export default function(state = initailState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
